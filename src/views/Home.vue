a595a5dce1080b06db574b424b0a1bce21ef57c3
wuchuheng <wuchuheng@163.com>
2020-05-31 01:06:22 +0800

    【config】修改eslink

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,18 +5,18 @@
 </template>
 
 <script>
-  import Epub from 'epubjs'
-  global.ePub = Epub
+import Epub from 'epubjs'
+global.ePub = Epub
 
-  export default {
-    name: 'Home',
-    components: {},
-    mounted() {
-      this.book = new ePub('/2018_Book_AgileProcessesInSoftwareEngine.epub')
-      this.book.renderTo('read', {
-        width: window.innerWidth,
-        height: window.innerHeight
-      }).display()
-    }
+export default {
+  name: 'Home',
+  components: {},
+  mounted() {
+    this.book = new Epub('/2018_Book_AgileProcessesInSoftwareEngine.epub')
+    this.book.renderTo('read', {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display()
   }
+}
 </script>
